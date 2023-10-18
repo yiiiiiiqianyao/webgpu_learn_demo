88b8da954edeb8b96800a1dca281279cd7046237
shihui <yiqianyao.yqy@alibaba-inc.com>
2023-10-18 19:31:07 +0800

    feat: add img load

---
+++ src/demos/utils.ts
@@ -0,0 +1,5 @@
+export async function loadImageBitmap(url: string) {
+    const res = await fetch(url);
+    const blob = await res.blob();
+    return await createImageBitmap(blob, { colorSpaceConversion: 'none' });
+}
